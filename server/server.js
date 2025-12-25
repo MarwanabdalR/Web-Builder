6f5a5887aeeaaefbc2eca7460a6afa9e4888f546
Marwan <marwanabdalrady13@gmail.com>
2025-12-25 07:51:45 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -60,24 +60,17 @@ const limiter = rateLimit({
 
 const app = express();
 const allowedOrigins = [
-  'http://localhost:5173',                  
-  'https://vibe-coder.vercel.app',          
-  'https://your-project-name.vercel.app'  
+  'http://localhost:5173',
+  'https://web-builder-mauve.vercel.app/'
 ];
 
 // Middleware
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'OPTIONS'],      
-  allowedHeaders: ['Content-Type', 'Authorization'], 
-  credentials: true,                        
-  optionsSuccessStatus: 200                 
+  origin: true, // Allow all origins
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  optionsSuccessStatus: 200
 }));
 
 app.options('*', cors());
